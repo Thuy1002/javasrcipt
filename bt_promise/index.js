9d2d5863441b4b96517012a71f2b7ccb6f1ac568
thuy1002 <thuy1002dangthanh@gmail.com>
2024-05-09 02:37:15 +0700

    update bt promise

--- bt_promise/index.js
+++ bt_promise/index.js
@@ -1,49 +1,64 @@
-//bt promise đợi 3 cái api để trả về kết quả
+//bt promise đợi 3 cái api để trả về kết quả 
 // cùng sử dụng như vậy nhưng dùng es6,7 (asyn wait)
 // -> mô tả luồng chạy
 // -> só sánh cú pháp cách sử dụng
 //  - note ra các key trong promise 
 /****/
 
-// function fetchData(url) {
-//     return fetch(url).then((data) => {
-//         return data.json()
-//     });
-// }
-// const url1 = fetchData('https://jsonplaceholder.typicode.com/todos/1');
-// const url2 = fetchData('https://jsonplaceholder.typicode.com/comments/1');
-// const url3 = fetchData('https://jsonplaceholder.typicode.com/albums/1');
+function fetchData(url) {
+    return fetch(url).then((res) => {
+        return res.json() //promise
+    }).then(data => {
+        return data; //data
+    }).catch(error => {
+        console.log(error)
+        return [];
+    });
+}
+const url1 = fetchData('https://jsonplaceholder.typicode.com/todos/1');
+const url2 = fetchData('https://jsonplaceholder.typicode.com/comments/1');
+const url3 = fetchData('https://jsonplaceholder.typicode.com/albums/1');
 
-// Promise.all([url1, url2, url3])
-//     .then(data => {
-//         console.log(data);
-//     }).catch((erro) => {
-//         console.log(erro);
-//     });
+Promise.all([url1, url2, url3])
+    .then(data => {
+        console.log(data);
+    }).catch((error) => {
+        console.log(error);
+        return undefined;
+    });
 
 
 // /***dùng es6 */
 
-const getData = async (data) => {
-    try {
-        const res = await fetch(data);
-        return res.json();
-    } catch (erro) {
-        return erro;
-    }
-}
+// const getData = async (url) => {
+//     try {
+//         const res = await fetch(url);
+//         const data = await res.json();
+//         return data;
 
-const promiseAll = async () => {
-    try {
-        const data1 = await getData('https://jsonplaceholder.typicode.com/todos/1');
-        const data2 = await getData('https://jsonplaceholder.typicode.com/comments/1');
-        const data3 = await getData('https://jsonplaceholder.typicode.com/albums/1');
+//     } catch (error) {
+//         console.log(error);
+//         return [];
+//     }
+// }
 
-        const result = await Promise.all([data1, data2, data3]);
-        console.log(result);
+// const promiseAll = async () => {
+//     try {
+//         const urlPromise1 = getData('https://jsonplaceholder.typicode.com/todos/1');
+//         const urlPromise2 = getData('https://jsonplaceholder.typicode.com/comments/1');
+//         const urlPromise3 = getData('https://jsonplaceholder.typicode.com/albums/1');
 
-    } catch (erro) {
-        console.log(erro);
-    }
-}
-promiseAll();
+//         const result = await Promise.all([urlPromise1, urlPromise2, urlPromise3]); //
+//         // Promise.all chờ cả
+//         console.log(result);
+
+//     } catch (error) {
+//         console.log(error);
+//         return undefined;
+//     }
+// }
+// promiseAll();
+
+
+// cách sủ dụng try...catch, note só sánh es7, es5,
+//luonggggg
---
+++ bt_promise/promise.md
@@ -0,0 +1,16 @@
+ **Promise**
+
+Promise là hàm sử lý bất động bộ
+- nó có 3 trạng thái: pendding, resolved, rejected
+
+* Cú pháp
+const promise = new Promise((resolved,rejected))=>{
+    resolved....
+    rejected...
+}
+- trong đó resolved là thành công || rejected là lỗi
+
+* Phương thức:promise.then() được thực hiện khi một Promise thành công hoặc bị lỗi.
+
+
+
